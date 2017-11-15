eb65f22a60248335355737e4075ba42a7e1497be
Philipp Oppermann <dev@phil-opp.com>
2017-11-15 12:53:12 +0100

    Allow trailing commas in enum_with_unknown macro

--- src/macros.rs
+++ src/macros.rs
@@ -36,7 +36,7 @@ macro_rules! enum_with_unknown {
     (
         $( #[$enum_attr:meta] )*
         pub enum $name:ident($ty:ty) {
-            $( $variant:ident = $value:expr ),+
+            $( $variant:ident = $value:expr ),+ $(,)*
         }
     ) => {
         enum_with_unknown! {
@@ -51,7 +51,7 @@ macro_rules! enum_with_unknown {
         pub doc enum $name:ident($ty:ty) {
             $(
               $( #[$variant_attr:meta] )+
-              $variant:ident = $value:expr
+              $variant:ident = $value:expr $(,)*
             ),+
         }
     ) => {
